1c1859eaccd4b2252b3463b2677c6bb628769f86
jaebooker <jaesonbooker@gmail.com>
2019-05-12 02:59:23 -0700

    added waypoint controller class

---
+++ OnTheRoadAgain/WayPointsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WayPointsViewController.swift
+//  OnTheRoadAgain
+//
+//  Created by Jaeson Booker on 5/12/19.
+//  Copyright © 2019 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class WayPointsViewController: UIViewController {
+
+    @IBAction func wayPointButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
