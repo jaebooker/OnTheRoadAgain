e69c213a99a0a9c3c876a793c3ac9ac3849964b6
jaebooker <jaesonbooker@gmail.com>
2019-05-12 02:09:42 -0700

    view controller class

---
+++ OnTheRoadAgain/AddTripViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddTripViewController.swift
+//  OnTheRoadAgain
+//
+//  Created by Jaeson Booker on 5/12/19.
+//  Copyright © 2019 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class AddTripViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
