beb12b658b48bb731622b24198c581fc7cfd105a
jaebooker <jaesonbooker@gmail.com>
2019-05-12 19:34:12 -0700

    added connections

--- OnTheRoadAgain/MapViewController.swift
+++ OnTheRoadAgain/MapViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-
+import MapKit
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
